45ceca2bc3b55cbf3b27766b4dcd58b0d50fb2a3
FreyVik <73382763+FreyVik@users.noreply.github.com>
2026-03-23 18:46:27 +0100

    Day 1 print shit. (#1)

---
+++ scripts/prints.py
@@ -0,0 +1,25 @@
+print("A", "B", "C", sep="-")
+print("End line", end=" ... end")
+print("""
+Multi linear
+text with
+\"\"\"
+""")
+
+print("-" * 15)
+print("Ja" + "ja" * 3)
+print("ABCDEFG"[0])
+print("ABCDEFG"[-1])
+print("ABCDEFG"[2:4])
+print("ABCDEFG")
+print("ABCDEFG"[:4])
+print("ABCDEFG"[2:])
+print("ABCDEFG")
+print("ABCDEFG"[-4])
+print("ABCDEFG"[:-4])
+print("ABCDEFG"[-4:])
+print(len("ABCDEFG"))
+
+print("-" * 15)
+
+print("You write: " + input("Write something: "))
