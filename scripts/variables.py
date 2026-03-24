cf83aa4eedfe8520b5739f3c00090c1fd3de0670
FreyVik <73382763+FreyVik@users.noreply.github.com>
2026-03-24 20:02:12 +0100

    day 2 variables (#4)
    Signed-off-by: FreyVik <yonkigamer96@gmail.com>

---
+++ scripts/variables.py
@@ -0,0 +1,36 @@
+name = "name"
+name = "Frey"
+print(name)
+
+age = 30
+print(name, " - ", age)
+
+# age = int(input("Update your age: "))
+print(name, " - ", age)
+
+number = 1
+print(number)
+print(type(number))
+float_number = 2.5
+print(float_number)
+print(type(float_number))
+print(number + float_number)
+print(type(number + float_number))
+
+print("Hello {}, you're {} years old".format(name, age))
+print(f"Hello {name}, you're {age} years old.")
+
+operation_number = 9
+print(20 / operation_number)
+print(10 // operation_number)
+print(10 % operation_number)
+print(operation_number * 2)
+print(operation_number ** 2)
+print(9 ** (1/2))
+print(10/3)
+print(round(10/3, 3))
+print(round(2.56))
+print(round(2.56, 1))
+print(round(2.56, 2))
+print(round(10/2))
+print(round(10/2, 0))
